39c7b02ace8e1f47c1599600b6a6fa0ce50238e5
PABLO DE JESUS CAMPOS VIANA <pablodejesus.campos.viana@bbvadata.com>
2020-04-05 21:40:27 +0200

    added threshold to rdds pipe approach

--- get_predictions.sh
+++ get_predictions.sh
@@ -6,7 +6,7 @@ while read LINE; do
    echo ${LINE}
 done > $filename.input
 
-./fastText/build/fasttext predict models/ft_tuned.ftz $filename.input 1 | sed 's/__label__//g' > $filename.preds
+./fastText/build/fasttext predict models/ft_tuned.ftz $filename.input 1 0.10 | sed 's/__label__//g' > $filename.preds
 
 paste -d ',' $filename.input $filename.preds > $filename.output && rm $filename.input $filename.preds
 cat $filename.output
